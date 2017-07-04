293b0a15971b45ff3a15a134b8ee065cba35727d
Bernardino Guerrero Ibarra <bernardino.guerrero@icloud.com>
2017-07-04 12:22:18 -0500

    IP was changed

--- SellerAppNetworkingMW/BackendUrlManager.swift
+++ SellerAppNetworkingMW/BackendUrlManager.swift
@@ -32,8 +32,9 @@ class BackendUrlManager : NSObject{
     fileprivate var serviceUrls:[String] = [String]();
     
     fileprivate func createUrls() {
-        //let backendHost = "172.22.209.88:9090/"
-        let backendHost = "172.22.49.66:9090/"
+        let backendHost = "172.22.209.88:9090/"
+        
+        //let backendHost = "172.22.49.66:9090/"
         let backendProtocol = "http://"
         let servicesCount = BackendUrlManager.SERVICE_CONTEXT.count
         for index in 0..<servicesCount {
