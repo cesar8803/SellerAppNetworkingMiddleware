33eb1239e3063efe27cb8f90180f2de1b09b0f42
Bernardino Guerrero Ibarra <bernardino.guerrero@gmail.com>
2017-08-29 16:08:41 -0500

    last changes

---
+++ SellerAppNetworkingMW/MWUrlHandler.swift
@@ -0,0 +1,15 @@
+//
+//  UrlHandlerMW.swift
+//  SellerAppNetworkingMW
+//
+//  Created by Bernardino Guerrero Ibarra on 8/29/17.
+//  Copyright © 2017 Liverpool. All rights reserved.
+//
+
+import Foundation
+
+public class MWUrlHandler{
+    private init(){}
+    public var url:String = ""
+    static public let sharedInstance = MWUrlHandler()
+}
