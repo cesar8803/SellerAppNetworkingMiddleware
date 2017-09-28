569092d5eb192b732b7a994befe772e973c0773a
Alejandro Hernandez <alejhernandez@deloittemx.com>
2017-09-28 15:36:45 -0500

    AlexH: Unified options

---
+++ SellerAppNetworkingMW/BenefitOptions.swift
@@ -0,0 +1,27 @@
+//
+//  BenefitOptions.swift
+//  SellerAppNetworkingMW
+//
+//  Created by Alejandro Hernandez on 28/09/17.
+//  Copyright © 2017 Liverpool. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public class BenefitOptions: Mappable{
+    
+    public var items: [Items]?
+    public var option: Int?
+    public var optionPrice: String?
+    
+    required public init?(map: Map){
+    }
+    
+    public func mapping(map: Map){
+        items <- map["items"]
+        option <- map["option"]
+        optionPrice <- map["optionPrice"]
+    }
+}
+
