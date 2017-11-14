6ba7d9aa5e62c910a1090ad7a1ac917ac2728796
JJMaqueda <jjmaquedaf@liverpool.com.mx>
2017-11-14 17:24:10 -0600

    Nuevo Objeto SOMS
    Se marea nuevo objeto SOMS, para respuesta en PDP

---
+++ SellerAppNetworkingMW/InventarioSOMS.swift
@@ -0,0 +1,27 @@
+//
+//  InventarioSOMS.swift
+//  SellerAppNetworkingMW
+//
+//  Created by Josue Maqueda Flores on 14/11/17.
+//  Copyright © 2017 Liverpool. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public class InventarioSOMS: Mappable
+{
+    public var getConsultaSkuPoolResponse   : GetConsultaSkuPoolResponse?
+    public var isSuccessful                 : Bool?
+    
+    public required init?(map: Map)
+    {
+    }
+    
+    public func mapping(map: Map){
+        getConsultaSkuPoolResponse <- map["GetConsultaSkuPoolResponse"]
+        isSuccessful <- map["isSuccessful"]
+    }
+    
+}
+
