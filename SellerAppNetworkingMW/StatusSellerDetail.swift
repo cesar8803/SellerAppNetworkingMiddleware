58c0bbd459cb7e194271663101c730cc2a1d6fc5
Emmanuel Casañas Cruz <emmanuel.c19@gmail.com>
2018-08-22 16:06:00 -0500

    Missing file

---
+++ SellerAppNetworkingMW/StatusSellerDetail.swift
@@ -0,0 +1,9 @@
+//
+//  StatusSellerDetail.swift
+//  SellerAppNetworkingMW
+//
+//  Created by Emmanuel Casanas Cruz on 22/08/18.
+//  Copyright © 2018 Liverpool. All rights reserved.
+//
+
+import Foundation
